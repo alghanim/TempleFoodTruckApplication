c406248778c6878aea5e716c7cb1714a0183142f
Rajinder Kumar <rajinder.kumar@temple.edu>
2014-11-26 16:45:54 -0600

    Load Maps and created a button to find the nearest trucks to the user

--- TempleFoodTruckApplication/src/com/example/templefoodtruckapplication/MainActivity.java
+++ TempleFoodTruckApplication/src/com/example/templefoodtruckapplication/MainActivity.java
@@ -1,17 +1,66 @@
 package com.example.templefoodtruckapplication;
 
-import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 
+public class MainActivity extends FragmentActivity implements LocationListener{
+
+	private GoogleMap googleMap;
+	private int numOfTrucks = 10;
+	LocationManager locationManager;
+	String locationProvider;
+	Marker posMarker;
+	Location userLocation;
+	LatLng truckLocation;
+	LatLng[] truckLocations = new LatLng[numOfTrucks];
+	double[] truckDistanceFromUser = new double[numOfTrucks];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		getMap();
+	}
+	
+	// sets up the map
+	private void getMap() {
+		FragmentManager fm = getSupportFragmentManager();
+		SupportMapFragment fragment = (SupportMapFragment) fm
+				.findFragmentById(R.id.map);
+		googleMap = fragment.getMap();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
 		
-		//testing
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
 	}
 }
